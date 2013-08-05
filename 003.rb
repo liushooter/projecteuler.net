6e42fadcc1119a03b23981e29e302d4acb0e59ee
shooterman <shooter.hero@gmail.com>
2013-08-06 00:25:22 +0800

    add 003 problem

---
+++ 003.rb
@@ -0,0 +1,27 @@
+# Largest prime factor
+# Published on Friday, 2nd November 2001, 06:00 pm; Solved by 186318
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143 ?
+
+# 600851475143 的最大质因数是多少
+#[求质数算法的N种境界](http://blog.csdn.net/program_think/article/details/7032600)
+#[How do I generate the first n prime numbers](http://stackoverflow.com/questions/11673968/how-do-i-generate-the-first-n-prime-numbers)
+
+  def prime_division num
+    a,b = 0
+    result = []
+    temp = Math.sqrt(num).to_i
+
+    2.upto(temp) do |n|
+      while num % n == 0
+        num /= n
+        result << n
+      end
+    end
+    result << num unless num == 1
+    result
+    puts "prime = #{result.max}"
+  end
+
+prime_division(600851475143)
