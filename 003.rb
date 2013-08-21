344d2b806053ee4e7c4ce0acfbbf5ae4057421f2
shooterman <shooter.hero@gmail.com>
2013-08-21 23:02:03 +0800

    modify 003.rb file

--- 003.rb
+++ 003.rb
@@ -1,5 +1,4 @@
 # Largest prime factor
-# Published on Friday, 2nd November 2001, 06:00 pm; Solved by 186318
 # The prime factors of 13195 are 5, 7, 13 and 29.
 
 # What is the largest prime factor of the number 600851475143 ?
