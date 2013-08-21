d5afa3aaa3cf0cccd7a536b2d8341a0a972d1fb4
shooterman <shooter.hero@gmail.com>
2013-08-22 01:01:12 +0800

    update

--- 006.rb
+++ 006.rb
@@ -1,17 +1,17 @@
 # Sum square difference
 # The sum of the squares of the first ten natural numbers is,
 
-# 12 + 22 + ... + 102 = 385
+# 1^2 + 2^2 + ... + 10^2 = 385
 # The square of the sum of the first ten natural numbers is,
 
-# (1 + 2 + ... + 10)2 = 552 = 3025
+# (1 + 2 + ... + 10)^2 = 552 = 3025
 # Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
 
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum
 
-# 前十个自然数的平方和是：12 + 22 + ... + 102 = 385
+# 前十个自然数的平方和是：1^2 + 2^2 + ... + 10^2 = 385
 
-# 前十个自然数的和的平方是：(1 + 2 + ... + 10)2 = 552 = 3025
+# 前十个自然数的和的平方是：(1 + 2 + ... + 10)^2 = 552 = 3025
 
 # 所以平方和与和的平方的差是 3025 - 385 = 2640.
 
--- 016.rb
+++ 016.rb
@@ -1,11 +1,11 @@
 # Power digit sum
-# 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
 # What is the sum of the digits of the number 21000?
 
-# 215 = 32768 并且其各位之和为 is 3 + 2 + 7 + 6 + 8 = 26.
+# 2^15 = 32768 并且其各位之和为 is 3 + 2 + 7 + 6 + 8 = 26.
 
-# 21000 的各位数之和是多少？
+# 2^1000 的各位数之和是多少？
 
 num = 2**1000
 array = num.to_s.split("")
